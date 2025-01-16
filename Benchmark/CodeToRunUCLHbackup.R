566153e6e0e93ee68c427471b419d55e3233e448
South <Andrew.South@UCLH201.NET>
2025-01-16 18:28:58 +0000

    move db connection details to .Renviron and gitignore

--- Benchmark/CodeToRunUCLHbackup.R
+++ Benchmark/CodeToRunUCLHbackup.R
@@ -17,21 +17,23 @@ dbName <- "UCLH-6months"
 cdmSchema <- "data_catalogue_003" #6 months
 
 
-# create a DBI connection to your database
-# user-prompt here, in script to run they are hardcoded
-user <- rstudioapi::askForPassword("user")
-host <- rstudioapi::askForPassword("host")
-port <- 15432
-dbname <- "omop_reservoir"
+# create a DBI connection to UCLH database
+# using credentials in .Renviron or you can replace with hardcoded values here
+user <- Sys.getenv("user")
+host <- Sys.getenv("host")
+port <- Sys.getenv("port")
+dbname <- Sys.getenv("dbname")
+# schema in database where you have writing permissions
+writeSchema <- "_other_andsouth"
+
+if("" %in% c(user, host, port, dbname, writeSchema))
+  stop("seems you don't have (all?) db credentials stored in your .Renviron file, use usethis::edit_r_environ() to create")
 pwd <- rstudioapi::askForPassword("Password for omop_db")
+
 con <- DBI::dbConnect(RPostgres::Postgres(),user = user, host = host, port = port, dbname = dbname, password=pwd)
 
-#get this if not connected to VPN
+#you get this if not connected to VPN
 #Error: could not translate host name ... to address: Unknown host
-
-
-# schema in the database where you have writing permissions
-writeSchema <- "_other_andsouth" #"data_catalogue_003"
 
 # created tables will start with this prefix
 prefix <- "uclh_hdruk_benchmark"
@@ -59,13 +61,4 @@ cdm <- CDMConnector::cdmFromCon(
 # run study code
 #source("Benchmark/R/RunBenchmark.R")
 source("R/RunBenchmark.R")
-
-# initial renv error
-# Error: This project does not contain a lockfile.
-# Have you called `snapshot()` yet?
-# renv::snapshot() not CDMConnector::snapshot() :-)
-#
-# error when I was running from home folder
-# In file(file, ifelse(append, "a", "w")) :
-# cannot open file 'HDRUK-Benchmark/Results//log_UCLH-6months_11_12_2024_18_35_54.txt': No such file or directory
 
